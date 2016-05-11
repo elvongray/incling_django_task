03435ddfd764c71b144929e138ef6096404d9ed0
Arinde Eniola <eniolaarinde1@gmail.com>
2016-05-11 23:41:44 +0100

    add the three models

---
+++ school/categories.py
@@ -0,0 +1,10 @@
+Gender_Categories = (
+    ('M', 'All Male'),
+    ('F', 'All Female'),
+    ('C', 'Coeducational'),
+)
+
+School_Categories = (
+    ('public_school', 'Public School'),
+    ('private_school', 'Private School'),
+)
--- school/models.py
+++ school/models.py
@@ -1,5 +1,43 @@
+
+"""
+A simple representation of a school, classroom and student models
+with the relationships between them.
+"""
+
 from __future__ import unicode_literals
 
+from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 
-# Create your models here.
+from school.categories import Gender_Categories, School_Categories
+
+
+@python_2_unicode_compatible
+class School(models.Model):
+    name = models.CharField(max_length=200, unique=True, blank=False)
+    address = models.TextField(blank=False)
+    date_established = models.DateField()
+    gender_allowed = models.CharField(choices=Gender_Categories)
+    school_type = models.CharField(choices=School_Categories)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+
+@python_2_unicode_compatible
+class Classroom(models.Model):
+    name = models.CharField(max_length=200, unique=True, blank=False)
+    school = models.ForeignKey(School)
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+
+@python_2_unicode_compatible
+class Student(models.Model):
+    name = models.CharField(max_length=200, unique=True, blank=False)
+    classroom = models.ForeignKey(Classroom)
+
+    def __str__(self):
+        return '{}'.format(self.name)
